6ac0fbcce6d2c3c217786170406619c93f99ef27
Spencer Krum <nibz@spencerkrum.com>
2020-03-09 21:53:48 +0000

    Pull data live from website

--- parse.js
+++ parse.js
@@ -1,29 +1,23 @@
 const cheerio = require('cheerio')
+const axios = require('axios')
 
-var fs = require('fs');
-fs.readFile( __dirname + '/situation.html', function (err, data) {
-  if (err) {
-    throw err; 
-  }
+axios.get("https://www.health.state.mn.us/diseases/coronavirus/situation.html")
+  .then(function (response) {
+    const $ = cheerio.load(response.data.toString());
 
-  const $ = cheerio.load(data.toString());
+    var positive_cases = $('td').eq(1).text()
+    var negative_cases = $('td').eq(3).text()
+    var total_cases = $('td').eq(5).text()
+    //console.log("Positive Cases " + positive_cases);
+    //console.log("Negative Cases " + negative_cases);
+    //console.log("Total Cases " + total_cases);
+    
+    var results = {
+      "positive_cases": positive_cases,
+      "negative_cases": negative_cases,
+      "total_cases": total_cases
+    };
 
-  var tb = $('table').html();
-  console.log(tb);
-  var positive_cases = $('td').eq(1).text()
-  var negative_cases = $('td').eq(3).text()
-  var total_cases = $('td').eq(5).text()
-  console.log("Positive Cases " + positive_cases);
-  console.log("Negative Cases " + negative_cases);
-  console.log("Total Cases " + total_cases);
-  
-  var results = {
-    "positive_cases": positive_cases,
-    "negative_cases": negative_cases,
-    "total_cases": total_cases
-  };
-
-  console.log(results)
-
-});
+    console.log(results)
+  });
 
