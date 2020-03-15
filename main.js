f16b23b4725ecba9f9470750672bf2726ffc7fca
Spencer Krum <nibz@spencerkrum.com>
2020-03-15 05:07:03 +0000

    New State: Add Oregon

--- main.js
+++ main.js
@@ -149,7 +149,38 @@ var managers = {
           });
       }
     },
-    "or": {},
+    "or": {
+      config: {
+          url: "https://www.oregon.gov/oha/PH/DISEASESCONDITIONS/DISEASESAZ/Pages/emerging-respiratory-infections.aspx",
+      },
+      updater: function(config) {
+        axios.get(config.url)
+          .then(function (response) {
+            const $ = cheerio.load(response.data.toString());
+            var summary = $('.ExternalClass23E56795FBF0468C9F856CD297450134 ');
+
+            temp_result = {
+              "positive_cases": parseFloat(summary.find('td').eq(2).text()),
+              "negative_tests": parseFloat(summary.find('td').eq(4).text()),
+              "pending_tests": parseFloat(summary.find('td').eq(6).text()),
+
+            };
+            updated_data = checkDataUpdate(temp_result, "or", state)
+            temp_result["updated_data"] = updated_data;
+
+            if (discord_post && updated_data) {
+              axios.post(process.env.DISCORD_WEBHOOK_URL, {
+                  content: `New Oregon Coronavirus Data: \nPositive: ${temp_result.positive_cases}\nNegative tests: ${temp_result.negative_tests}\nPending Tests: ${temp_result.pending_tests}\n`
+              })
+            }
+
+            storeState("or", temp_result);
+          })
+          .catch(function (error) {
+            console.log("Failed to get or cases: " + error);
+          });
+      },
+    },
     "pa": {},
     "tx": {}
 }
@@ -194,7 +225,10 @@ for (let [state, manager] of Object.entries(managers)) {
 }
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/cases/all', (req, res) => res.send(result))
 app.get('/cases/mn', (req, res) => res.send(result.mn))
+app.get('/cases/ny', (req, res) => res.send(result.ny))
+app.get('/cases/or', (req, res) => res.send(result.or))
 app.get('/cases/ri', (req, res) => res.send(result.ri))
 app.get('/cases/fed', (req, res) => res.send(result.fed))
 
@@ -225,6 +259,12 @@ client.on('message', msg => {
 client.on('message', msg => {
   if (msg.content === '!ri') {
     msg.reply(`Rhode Island Coronavirus Data: \nPositive: ${state.ri.positive_cases}\nNegative tests: ${state.ri.negative_tests}\nPending Tests: ${state.ri.pending_tests}\nUnder Quarantine: ${state.ri.quarantine}`)
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!or') {
+    msg.reply(`Oregon Coronavirus Data: \nPositive: ${state.or.positive_cases}\nNegative tests: ${state.or.negative_tests}\nPending Tests: ${state.or.pending_tests}\n`)
   }
 });
 
